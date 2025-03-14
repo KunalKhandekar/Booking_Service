40388b82eaad18a56a8cd71a9f832e3180e5700a
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-03-14 12:19:24 +0530

    Added booking table

---
+++ src/utils/common/enums.js
@@ -0,0 +1,15 @@
+const Seats = {
+  BUSINESS: "business",
+  ECONOMY: "economy",
+  PREMIUM_ECONOMY: "premium_economy",
+  FIRST_CLASS: "first_class",
+};
+
+const BOOKING_STATUS = {
+  BOOKED: "booked",
+  CANCELLED: "cancelled",
+  INITIATED: "initiated",
+  PENDING: "pending",
+};
+
+module.exports = { Seats, BOOKING_STATUS };
